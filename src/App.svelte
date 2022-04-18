fa74fd3b1e48fb2b707b91591f53b6cef4c09bb5
Amber Yang <a227yang@uwaterloo.ca>
2022-04-18 19:35:40 -0400

    Svelte-Snowpack Setup

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let name = 'Hello Svelte!'
+</script>
+<h1>{name}</h1>
+<button class="btn btn-primary">
+  TEST!
+</button>
+
+<style lang="scss">
+  $color: red;
+  h1 {
+    color: $color;
+  }
+  .heropy {
+    color: red;
+  }
+</style>
